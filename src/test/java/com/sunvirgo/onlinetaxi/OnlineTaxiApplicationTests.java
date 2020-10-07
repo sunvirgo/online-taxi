0ddd7119c494c29628f017850cb7bd61b5dbbfea
sunvirgo <g_sun2java@163.com>
2020-10-07 21:26:21 +0800

    Ustinian:day01_新建网约车项目

---
+++ src/test/java/com/sunvirgo/onlinetaxi/OnlineTaxiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sunvirgo.onlinetaxi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineTaxiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
